111214963bd2c23e656fab151090d3cbd911df16
Bright <barhin49@gmail.com>
2020-04-07 10:31:13 +0000

    extracted JWT SECRETS to env file

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,4 +1,4 @@
 import jwt from 'jsonwebtoken'
-const generateToken = userId => jwt.sign({userId}, 'thisisasecret', {expiresIn : '7 days'})
+const generateToken = userId => jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn : '7 days'})
 
 export default generateToken;
